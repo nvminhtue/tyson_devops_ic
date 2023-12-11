08154ae71be3bd9b51f34ab710721153b8702b75
nvminhtue <nvminhtue@gmail.com>
2023-12-11 22:45:00 +0700

    [gh-16] Update naming and position

--- modules/iam/groups/main.tf
+++ modules/iam/groups/main.tf
@@ -5,13 +5,13 @@ resource "aws_iam_group" "admin" {
 }
 
 # trivy:ignore:AVD-AWS-0123
-resource "aws_iam_group" "bot" {
-  name = "Bot-group"
+resource "aws_iam_group" "developer" {
+  name = "Developer-group"
 }
 
 # trivy:ignore:AVD-AWS-0123
-resource "aws_iam_group" "developer" {
-  name = "Developer-group"
+resource "aws_iam_group" "bot" {
+  name = "Bot-group"
 }
 
 // Define policies
@@ -29,15 +29,15 @@ resource "aws_iam_group_policy" "bot_full_iam_access" {
 // Define policy attachments
 resource "aws_iam_group_policy_attachment" "admin_access" {
   group      = aws_iam_group.admin.name
-  policy_arn = data.aws_iam_policy.admin_policy.arn
+  policy_arn = data.aws_iam_policy.admin.arn
 }
 
 resource "aws_iam_group_policy_attachment" "developer_power_user_access" {
   group      = aws_iam_group.developer.name
-  policy_arn = data.aws_iam_policy.power_user_policy.arn
+  policy_arn = data.aws_iam_policy.power_user.arn
 }
 
 resource "aws_iam_group_policy_attachment" "bot_power_user_access" {
   group      = aws_iam_group.bot.name
-  policy_arn = data.aws_iam_policy.power_user_policy.arn
+  policy_arn = data.aws_iam_policy.power_user.arn
 }
