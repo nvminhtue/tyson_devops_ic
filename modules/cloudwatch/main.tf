da607ea8caac73873739721868cd57937f2c9bc2
nvminhtue <nvminhtue@gmail.com>
2023-12-19 21:56:08 +0700

    [gh-7] Fix typo

--- modules/cloudwatch/main.tf
+++ modules/cloudwatch/main.tf
@@ -1,4 +1,4 @@
 resource "aws_cloudwatch_log_group" "this" {
   name              = "devops-ic-${var.env_namespace}-log-group"
-  retention_in_days = locals.retention_in_days
+  retention_in_days = local.retention_in_days
 }
