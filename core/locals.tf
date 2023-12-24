0c3a270160c31a447332ae5c06916d102c956a05
nvminhtue <nvminhtue@gmail.com>
2023-12-24 17:10:20 +0700

    [gh-11] Add RDS module

--- core/locals.tf
+++ core/locals.tf
@@ -1,5 +1,6 @@
 locals {
-  app_name  = "devops_ic"
-  region    = "ap-southeast-1"
-  namespace = "${local.app_name}-${var.environment}"
+  app_name           = "devops_ic"
+  region             = "ap-southeast-1"
+  namespace          = "${local.app_name}-${var.environment}"
+  rds_instance_class = "db.t3.micro"
 }
