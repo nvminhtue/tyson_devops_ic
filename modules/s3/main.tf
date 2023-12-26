65db6a359708db8dcefdb1248743da8022d7d8b3
nvminhtue <nvminhtue@gmail.com>
2023-12-26 22:43:53 +0700

    [gh-9] Integrate S3 bucket

---
+++ modules/s3/main.tf
@@ -0,0 +1,33 @@
+resource "aws_s3_bucket" "this" {
+  bucket        = "${var.namespace}-bucket"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_acl" "this" {
+  bucket = aws_s3_bucket.this.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+  block_public_acls       = true
+  block_public_policy     = true
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "this" {
+  bucket = aws_s3_bucket.this.bucket
+  rule {
+    id = "expired_objects"
+
+    expiration {
+      days = var.expiration_in_days
+    }
+
+    status = "Enabled"
+  }
+}
+
+// TODO: Add s3 bucket policy for alb
