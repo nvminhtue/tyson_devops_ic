59047609860edf98baabcee82af918bc90a2f866
nvminhtue <nvminhtue@gmail.com>
2024-06-11 21:05:02 +0700

    [gh-6] Add random suffix on secret name

--- modules/ssm/main.tf
+++ modules/ssm/main.tf
@@ -1,7 +1,12 @@
 resource "aws_ssm_parameter" "secret_parameters" {
   for_each = var.secrets
 
-  name  = "/${var.env_namespace}/${each.key}"
+  name  = "/${var.env_namespace}/${each.key}-${random_string.service_secret_random_suffix.result}"
   type  = "String"
   value = each.value
+}
+
+resource "random_string" "service_secret_random_suffix" {
+  length  = 6
+  special = false
 }
