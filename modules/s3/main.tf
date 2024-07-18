2f1264f0c2da7207e6d657c55b44f66f65016b7f
nvminhtue <nvminhtue@gmail.com>
2024-07-18 22:28:46 +0700

    chore: Update S3 bucket

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket_ownership_controls" "this" {
   bucket = aws_s3_bucket.this.id
 
   rule {
-    object_ownership = "BucketOwnerEnforced"
+    object_ownership = "ObjectWriter"
   }
 }
 
