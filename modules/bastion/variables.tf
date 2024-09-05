824fc82b52890028ce3ad302bf39820cbd3eb8ba
nvminhtue <nvminhtue@gmail.com>
2024-09-05 21:52:39 +0700

    chore: update AMI ID

--- modules/bastion/variables.tf
+++ modules/bastion/variables.tf
@@ -35,5 +35,5 @@ variable "min_instance_count" {
 
 variable "ami_id" {
   description = "The AMI ID for the instance"
-  default     = "ami-0d5a0fb3057b73a85"
+  default     = "ami-0acbb557db23991cc"
 }
