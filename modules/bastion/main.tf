20e2d5c171b61a511e388a026453fc97abfeb837
nvminhtue <nvminhtue@gmail.com>
2024-09-06 16:54:41 +0700

    chore: Update instance specs

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -1,7 +1,7 @@
 resource "aws_launch_template" "bastion_instance" {
   name_prefix   = "${var.namespace}-bastion-"
   # image_id      = data.aws_ami.this.id
-  image_id      = var.ami_id
+  image_id      = "ami-0012ef68c16acc6f4"
   instance_type = var.instance_type
   # key_name      = "${var.namespace}-bastion"
 
