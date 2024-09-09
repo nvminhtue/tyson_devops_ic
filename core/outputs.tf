506d0aedd6ef6b2c6c377f0c3d72d3e354dae103
nvminhtue <nvminhtue@gmail.com>
2024-09-09 16:45:29 +0700

    chore: Ignore all services but bastion and SG

--- core/outputs.tf
+++ core/outputs.tf
@@ -1,11 +1,11 @@
-output "cluster_endpoint" {
-  value = module.rds.cluster_endpoint
-}
+# output "cluster_endpoint" {
+#   value = module.rds.cluster_endpoint
+# }
 
-output "cluster_id" {
-  value = module.rds.cluster_id
-}
+# output "cluster_id" {
+#   value = module.rds.cluster_id
+# }
 
-output "secrets_variables" {
-  value = module.ssm.secrets_variables
-}
+# output "secrets_variables" {
+#   value = module.ssm.secrets_variables
+# }
