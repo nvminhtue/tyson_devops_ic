1d87624b29d18767cc7817c06e18d41fdbb3afe6
nvminhtue <nvminhtue@gmail.com>
2024-09-09 18:26:52 +0700

    chore: Replace instance type with instance requirement

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -1,7 +1,7 @@
 resource "aws_launch_template" "bastion_instance" {
   name_prefix = "${var.namespace}-bastion-"
   # image_id      = data.aws_ami.this.id
-  image_id      = "ami-01811d4912b4ccb26"
+  image_id = "ami-01811d4912b4ccb26"
   # instance_type = var.instance_type
   # key_name      = "${var.namespace}-bastion"
 
@@ -59,9 +59,13 @@ resource "aws_autoscaling_group" "bastion_instance" {
   desired_capacity    = var.instance_desired_count
   vpc_zone_identifier = var.subnet_ids
 
-  launch_template {
-    id      = aws_launch_template.bastion_instance.id
-    version = aws_launch_template.bastion_instance.latest_version
+  mixed_instances_policy {
+    launch_template {
+      launch_template_specification {
+        launch_template_id = aws_launch_template.bastion_instance.id
+        version            = aws_launch_template.bastion_instance.latest_version
+      }
+    }
   }
 
   tag {
